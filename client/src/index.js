81be2aad8b12a22000e265f5ee304c8d0f77ae98
Kevin Bernowski <bernowski@gmx.de>
2025-05-09 15:28:24 +0200

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+//import logo from './logo.svg';
+import './App.css';
+import App from './App.js';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const container = document.getElementById('mydiv');
+      const root = ReactDOM.createRoot(container);
+      root.render(<App />)
---
+++ server/index.js
@@ -0,0 +1,32 @@
+// server/index.js
+
+const express = require("express");
+const path = require('path');
+
+
+const app = express();
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  console.log(`Request received at ${new Date().toISOString()} from ${req.ip}`);
+  res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React 
+
+
+app.get('/{*any}', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'fallback.html'));
+});
+
+const PORT = process.env.PORT || 3001;
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
